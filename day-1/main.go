03ec4cda8ce16c2ef437b517a05ac4daa2b1bf4d
jibesh <jibeshshrestha00@gmail.com>
2024-10-26 21:13:48 +0545

    day1 part1

--- day-1/main.go
+++ day-1/main.go
@@ -1,7 +1,89 @@
-package day1
+package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readFile(filename string) ([]string, error) {
+    file, err := os.Open(filename)
+    if err != nil {
+        return nil, err
+    }
+
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    
+    var lines []string
+
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+
+    return lines, nil
+}
+
+func findCalibrationValue(input []string) ([]int, error) {
+    var firstDigit, lastDigit int
+    var result []int
+
+    for _, value := range input {
+
+        for i := 0; i < len(value); i++ {
+            if strings.ContainsAny(string(value[i]), "0123456789") {
+                integer, err := strconv.Atoi(string(value[i]))
+                if err != nil {
+                    return nil, err
+                }
+                firstDigit = integer
+                break;
+            }
+        }
+
+        for i := len(value) - 1; i >= 0; i-- {
+            if strings.ContainsAny(string(value[i]), "0123456789") {
+                integer, err := strconv.Atoi(string(value[i]))
+                if err != nil {
+                    return nil, err
+                }
+                lastDigit = integer
+                break
+            }
+        }
+
+        var tempResult = firstDigit * 10 + lastDigit
+        result = append(result, tempResult)
+    }
+
+    return result, nil
+}
+
+func sumInt(input []int) (int) {
+    var result int
+    for _, integer := range input {
+        result += integer
+    }
+    return result
+}
 
 func main() {
-    fmt.Println("Hello World");
+    file, err := readFile("input.txt")
+    if err != nil {
+        log.Print(err)
+        os.Exit(1)
+    }
+
+    result, err := findCalibrationValue(file)
+    if err != nil {
+        log.Print(err)
+        os.Exit(1)
+    }
+    
+    sum := sumInt(result)
+    fmt.Println(sum)
 }
